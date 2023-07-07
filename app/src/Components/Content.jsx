a082ca0d48e1df2bbe3e3095cc149bc366faedd7
Rupantar <rupantarborphukan@gmail.com>
2023-07-07 21:36:13 +0530

    main page

--- app/src/Components/Content.jsx
+++ app/src/Components/Content.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
+import Round from '../Assets/Ellipse1.png';
+import bg from '../Assets/home-pic.png';
+import logo from '../Assets/logo.png';
+import rectangle from "../Assets/Rectangle.png";
 
 // Css
 import classes from '../Components/Content.module.css';
 
 function Content() {
   return (
-      <>
-        <h1>hello</h1>
-      </>
-  )
+    <div>
+      <div className={classes.maincontainer}>
+        <div className={classes.container}>
+          <img src={rectangle} alt="background" className={classes.mainbg} />
+          <img src={logo} alt="Logo" />
+          <h1>ENITIATE</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Content
